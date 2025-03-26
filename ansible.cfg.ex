4e30bd0646d6092141bae6fffe1a2e46284b76f0
chukbee1969 <charles.e.byrd1@gmail.com>
2025-03-26 15:34:31 +0000

    Using Roles

--- ansible.cfg.ex
+++
@@ -1,8 +0,0 @@
-[defaults]
-# default location of inventory
-# this can be a file or a directory
-inventory = /home/student/mycode/inv/dev/
-
-# prevents playbook from hanging on new connections
-host_key_checking = False
-
--- ansiblecfg/ansible.cfg.ex
+++ ansiblecfg/ansible.cfg.ex
@@ -6,8 +6,3 @@ inventory = /home/student/mycode/inv/dev/
 # prevents playbook from hanging on new connections
 host_key_checking = False
 
-# TURN ON COWSAY
-#nocows = 0    
-
-# TURN ON COWS AND OTHER RANDOM ASCII ART
-#cow_selection = random
